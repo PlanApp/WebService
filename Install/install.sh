1cb9342b198585fa5fe146d9ffb7fadd398812bf
Leonardo Bravo <leonardbravoillanes@gmail.com>
2014-09-05 10:51:10 -0400

    WebService - PlanApp V1.0

---
+++ Install/install.sh
@@ -0,0 +1,7 @@
+DEB
+#sudo apt-get install apache2 libapache2-mod-wsgi  libapache2-mod-python
+#sudo apt-get install python-flask
+
+RPM
+sudo yum -y install httpd mod_wsgi.x86_64  python-httplib2.noarch  python-rest-client.noarch  python-requests.noarch 
+sudo yum install python-flask
